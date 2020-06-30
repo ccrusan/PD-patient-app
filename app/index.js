703adcde56dca23919f847e7a535a6b677e1c453
ccrusan <67606407+ccrusan@users.noreply.github.com>
2020-06-30 11:56:48 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,27 +3,6 @@
 import document from "document";
 import { init } from "./views";
 
-//let tripButton = document.getElementById("tripButton");
-//tripButton.onactivate = function(evt) {
-//  console.log(localStorage.getItem("fallType"));
-  
-//}
-
-//let slipButton = document.getElementById("slipButton");
-//slipButton.onactivate = function(evt) {
-//  console.log("Fall type: slip");
-//}
-
-//let fallButton = document.getElementById("fallButton");
-//fallButton.onactivate = function(evt) {
-//  console.log("Fall type: fall");
-//}
-
-//let collapseButton = document.getElementById("collapseButton");
-//collapseButton.onactivate = function(evt) {
-//  console.log("Fall type: collapse");
-//}
-
 const views = init(
     [
         ["view-1", () => import("./views/view-1")],
