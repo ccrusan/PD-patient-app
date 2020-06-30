2fc2273c2e9fba4931f65381db4e70cb048d5d36
ccrusan <ccrusan@nd.edu>
2020-06-30 13:01:47 -0400

    update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,15 @@
 
 import document from "document";
 import { init } from "./views";
+import { HeartRateSensor } from "heart-rate";
+
+// Fetch UI elements we will need to change
+let hrLabel = document.getElementById("hrm");
+let updatedLabel = document.getElementById("updated");
+
+// Keep a timestamp of the last reading received. Start when the app is started.
+let lastValueTimestamp = Date.now();
+
 
 const views = init(
     [
@@ -11,7 +20,21 @@ const views = init(
     "./resources/views/"
 );
 
+// This function updates the heart rate printed to console.
+function updateDisplay() {
+    console.log("Current heart rate: " + hrm.heartRate);
+}
+
+// Create a new instance of the HeartRateSensor object
+var hrm = new HeartRateSensor();
+
 // Select the first view (view-1) after 1 second
 setTimeout(() => {
     views.navigate("view-1");
 }, 1000);
+
+// Begin monitoring the sensor
+hrm.start();
+
+// And update the display every second
+setInterval(updateDisplay, 1000);
